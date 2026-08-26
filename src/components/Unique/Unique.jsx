76ab02c2060653e3a0550ce89bdeb1d3d97d6c95
lotus-pocus <116104241+lotus-pocus@users.noreply.github.com>
2026-08-26 16:05:16 +0100

    added movement to unique png

--- src/components/Unique/Unique.jsx
+++ src/components/Unique/Unique.jsx
@@ -4,9 +4,7 @@ import "./Unique.css";
 const Unique = () => {
   return (
     <section className="unique-section">
-
       <div className="unique-content">
-
         {/* STAR / UNIQUE FUN GAMESHOW GRAPHIC */}
         <motion.img
           className="unique-star"
@@ -20,18 +18,32 @@ const Unique = () => {
           whileInView={{
             opacity: 1,
             scale: 1,
-            rotate: 0,
+            rotate: [-7, 7, -7],
           }}
           viewport={{
             once: true,
             amount: 0.4,
           }}
           transition={{
-            duration: 0.55,
-            ease: "easeOut",
+            opacity: {
+              duration: 0.9,
+              ease: "easeOut",
+            },
+
+            scale: {
+              duration: 0.55,
+              ease: "easeOut",
+            },
+
+            rotate: {
+              delay: 0.55,
+              duration: 2.4,
+              ease: "easeInOut",
+              repeat: Infinity,
+              repeatType: "loop",
+            },
           }}
         />
-
 
         {/* RIGHT-HAND TITLE */}
         <motion.h2
@@ -57,9 +69,7 @@ const Unique = () => {
           <span>JUST A</span>
           <span>GAMESHOW!</span>
         </motion.h2>
-
       </div>
-
     </section>
   );
 };
